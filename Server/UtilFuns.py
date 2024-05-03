ff2a8c17fcc7b5a13cf34f6e3b723d92a6d7634d
Omer Bartfeld <112820943+OmerBart@users.noreply.github.com>
2024-05-03 18:19:40 +0300

    Create UtilFuns.py
    started writing functions and logic for app local backend

---
+++ Server/UtilFuns.py
@@ -0,0 +1,39 @@
+# Importing necessary libraries
+import sounddevice as sd
+import soundfile as sf
+
+
+
+def grabAudioIO():
+    """
+    Function to grab audio input and output devices
+    """
+   
+
+    # Getting the list of audio devices
+    devices = sd.query_devices()
+    print(devices)
+
+    # Getting the input and output devices
+    input_device = int(input("Relevant Deceptify input "))
+    output_device = int(input("Relevant Deceptify output "))
+
+    # Returning the input and output devices
+    return input_device, output_device
+
+def recordConvo(input_device, output_device):
+    """
+    Function to record the conversation
+    """
+
+    # Getting the sample rate
+    sample_rate = 44100
+
+    # Getting the duration of recording
+    duration = 10
+
+    # Recording the conversation
+    recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2, device=(input_device, output_device))
+
+    # Returning the recording
+    return recording, sample_rate
